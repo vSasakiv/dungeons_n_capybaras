540ffa590cbbd1c618772e3a54b8625b9ffc5ffa
vSasakiv <vitorsakivenzelv@gmail.com>
2023-05-24 08:43:39 -0300

    :hammer: refactored App.java to use new structure

--- src/App.java
+++ src/App.java
@@ -1,24 +1,26 @@
+import gameloop.Constants;
+import gameloop.GameFrame;
+import gameloop.GameLoop;
+import gameloop.ThreadPool;
+
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 class App {
   public static void main(String[] args) {
-    // Inicializa a Threadpool
+    // Inicializa a Thread pool
     ThreadPool.init();
 
-    EventQueue.invokeLater(new Runnable() {
-      @Override
-      public void run() {
-        GameFrame frame = new GameFrame();
-        frame.setLocationRelativeTo(null); // inicia o GameFrame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.initBufferStrategy(); // inicia o buffering
-        // Cria e executa o game loop
-        GameLoop game = new GameLoop(frame);
-        game.start();
-        ThreadPool.execute(game);
-      }
+    EventQueue.invokeLater(() -> {
+      GameFrame frame = new GameFrame(Constants.WIDTH, Constants.HEIGHT);
+      frame.setLocationRelativeTo(null); // inicia o gameloop.GameFrame
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+      frame.initBufferStrategy(); // inicia o buffering
+      // Cria e executa o game loop
+      GameLoop game = new GameLoop(frame);
+      game.start();
+      ThreadPool.execute(game);
     });
   }
 }
