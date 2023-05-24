00058d711e544f5b9ce80e7a0b4f7c1cb071b059
vSasakiv <vitorsakivenzelv@gmail.com>
2023-05-24 09:36:34 -0300

    :sparkles: added handler for the mouse

---
+++ src/gameloop/MouseHandler.java
@@ -0,0 +1,40 @@
+package gameloop;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+class MouseHandler extends MouseAdapter {
+
+    private int mouseX, mouseY;
+    private boolean mousePress;
+    @Override
+    public void mousePressed(MouseEvent e) {
+        mouseX = e.getX();
+        mouseY = e.getY();
+        mousePress = true;
+    }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        mousePress = false;
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        mouseX = e.getX();
+        mouseY = e.getY();
+    }
+
+    public int getMouseX() {
+        return mouseX;
+    }
+
+    public int getMouseY() {
+        return mouseY;
+    }
+
+    public boolean isMousePress() {
+        return mousePress;
+    }
+}
