b4e9720e89b5d37ef6634d398a28c911a56c1fd5
vSasakiv <vitorsakivenzelv@gmail.com>
2023-05-24 08:42:40 -0300

    :sparkles: added Constants.java

---
+++ src/gameloop/Constants.java
@@ -0,0 +1,10 @@
+package gameloop;
+
+/**
+ * Classe que contém constantes utilizadas por todo o projeto
+ */
+public final class Constants {
+    private Constants(){}
+    public static final int HEIGHT = 720;
+    public static final int WIDTH = 16 * 720 / 9;
+}
