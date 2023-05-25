716e5491b5629beea6aa7ba757be0f10c71d3ffd
Vinicius <viniciusbsilva@usp.br>
2023-05-25 00:33:04 -0300

    :fire:  Removendo a implementação do ThreadPool

--- src/App.java
+++ src/App.java
@@ -1,15 +1,12 @@
 import gameloop.Constants;
 import gameloop.GameFrame;
 import gameloop.GameLoop;
-import gameloop.ThreadPool;
 
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 class App {
   public static void main(String[] args) {
-    // Inicializa a Thread pool
-    ThreadPool.init();
 
     EventQueue.invokeLater(() -> {
       GameFrame frame = new GameFrame(Constants.WIDTH, Constants.HEIGHT);
@@ -20,7 +17,6 @@ class App {
       // Cria e executa o game loop
       GameLoop game = new GameLoop(frame);
       game.start();
-      ThreadPool.execute(game);
     });
   }
 }
