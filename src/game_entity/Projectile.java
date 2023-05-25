a34c7a3e4e7c5b72569a93927ce0a602096b2e38
vSasakiv <vitorsakivenzelv@gmail.com>
2023-05-25 11:16:54 -0300

    :sparkles: added check for Projectile position so the projectile can be deleted

--- src/game_entity/Projectile.java
+++ src/game_entity/Projectile.java
@@ -1,5 +1,7 @@
 package game_entity;
 
+import gameloop.Constants;
+
 public class Projectile extends GameEntity{
     Vector direction;
     public Projectile(float posX, float posY, int velocity, Vector direction) {
@@ -11,7 +13,16 @@ public class Projectile extends GameEntity{
     public void tick() {
         this.position = Vector.add(this.position, Vector.scalarMultiply(direction, velocity));
     }
-
     @Override
     public void tick(Vector direction){}
+
+    /**
+     * @return boolean indicando se o projétil está fora do mapa e deve deixar de ser atualizado
+     */
+    public boolean outOfBounds(){
+        return  this.getWorldPosX() < 0 ||
+                this.getWorldPosX() > Constants.WORLD_WIDTH ||
+                this.getWorldPosY() < 0 ||
+                this.getWorldPosY() > Constants.WORLD_HEIGHT;
+    }
 }
