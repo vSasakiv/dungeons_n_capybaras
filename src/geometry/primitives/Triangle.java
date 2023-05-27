37bf2242fb0f8c677ff891f22ca7c2d56dd74bd3
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2023-05-26 23:46:27 -0300

    :fire: função de interioridade ponto-triângulo

--- src/geometry/primitives/Triangle.java
+++ src/geometry/primitives/Triangle.java
@@ -1,5 +1,7 @@
 package geometry.primitives;
 
+import geometry.*;
+
 public class Triangle {
     private Point A;
     private Point B;
@@ -25,6 +27,31 @@ public class Triangle {
     public boolean isInside(Point P) {
         //TODO
         boolean veredict = false;
+
+        Vector AB = new geometry.Vector(A, B);
+        Vector AC = new geometry.Vector(A, C);
+        Vector AP = new geometry.Vector(A, P);
+
+        
+        if(Vector.angleBetween(AC, AB) < Vector.angleBetween(AP, AB) + Vector.angleBetween(AP, AC)){
+            // verifica se P pertence à região angular definida por BÂC
+            veredict = false;
+        } else {
+            // verifica se o tamanho de AP não extrapola o triângulo
+            Vector projAB = Vector.projection(AP, AB);
+            Vector projAC = Vector.projection(AP, AC);
+
+            // AP = alpha*AB + beta*AC (decomposição)
+            float alpha, beta;
+
+            alpha = projAB.getModulus()/AB.getModulus();
+            beta = projAC.getModulus()/AC.getModulus();
+
+            if(alpha + beta > 1)
+                veredict = false;
+            else
+                veredict = true;
+        }
         return veredict;
     }
 }
