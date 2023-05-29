89438a64fb3351ab671d59dcf4826abc6239ebef
vSasakiv <vitorsakivenzelv@gmail.com>
2023-05-28 22:30:37 -0300

    :hammer: modified hitbox too better suit the game

--- src/game_entity/Hitbox.java
+++ src/game_entity/Hitbox.java
@@ -1,119 +1,75 @@
 package game_entity;
 
+import java.awt.*;
+
 public class Hitbox {
     // Implementação para uma região retangular
-    private float minX; // menor coordenada x do retângulo
-    private float maxX; // maior coordenada x do retângulo
-    private float minY; // menor coordenada y do retângulo
-    private float maxY; // maior coordenada y do retângulo
+
+    private Vector position;
+    private final float width;
+    private final float height;
+
 
     /**
-     * Construtor da Hitbox, com coordenadas já definidas
-     * @param minX menor coordenada x do retângulo
-     * @param maxX maior coordenada x do retângulo
-     * @param minY menor coordenada y do retângulo
-     * @param maxY maior coordenada y do retângulo
+     * @param width largura da hitbox
+     * @param height altura da hitbox
+     * @param position posição da entidade que irá conter a hitbox
      */
-    public Hitbox(float minX, float maxX, float minY, float maxY) {
-        this.minX = minX;
-        this.minY = minY;
-        this.maxX = maxX;
-        this.maxY = maxY;
+    public Hitbox(float width, float height, Vector position) {
+        this.position = position;
+        this.width = width;
+        this.height =  height;
     }
 
     /**
-     * Obtém menor coordenada x da hitbox
-     * @return minX
+     * @return obtém menor coordenada x da caixa
      */
-    public float getMinX() {
-        return minX;
+    public float minX(){
+        return -1 * this.width/2 + this.position.x;
     }
 
     /**
-     * Obtém menor coordenada y da hitbox
-     * @return minY
+     * @return obtém maior coordenada x da caixa
      */
-    public float getMinY() {
-        return minY;
+    public float maxX(){
+        return this.width/2 + this.position.x;
     }
 
     /**
-     * Obtém maior coordenada x da hitbox
-     * @return maxX
+     * @return obtém menor coordenada y da caixa
      */
-    public float getMaxX() {
-        return maxX;
+    public float minY(){
+        return -1 * this.height/2 + this.position.y;
     }
 
     /**
-     * Obtém maior coordenada y da hitbox
-     * @return maxY
+     * @return obtém maior coordenada y da caixa
      */
-    public float getMaxY() {
-        return maxY;
+    public float maxY(){
+        return this.width/2 + this.position.y;
     }
-
-    /**
-     * Define menor coordenada X da hitbox
-     * @param minX novo valor para minX
-     */
-    public void setMinX(float minX) {
-        this.minX = minX;
-    }
-
-    /**
-     * Define menor coordenada Y da hitbox
-     * @param minY novo valor para minY
-     */
-    public void setMinY(float minY) {
-        this.minY = minY;
-    }
-
-    /**
-     * Define maior coordenada x da hitbox
-     * @param maxX novo valor para maxX
-     */
-    public void setMaxX(float maxX) {
-        this.maxX = maxX;
-    }
-
-    /**
-     * Define maior coordenada y da hitbox
-     * @param maxY novo valor para maxY
-     */
-    public void setMaxY(float maxY) {
-        this.maxY = maxY;
-    }
-
-    /**
-     * Desloca a hitbox segundo um vetor de deslocamento
-     * @param displacement vetor de deslocamento
-     */
-    public void move(Vector displacement) {
-        setMinX(this.getMinX() + displacement.x);
-        setMaxX(this.getMaxX() + displacement.x);
-        setMinY(this.getMinY() + displacement.y);
-        setMaxY(this.getMaxY() + displacement.y);
-    }
-
     /**
      * Verifica se a hitbox está colidindo com outra
      * @param hitbox hitbox cuja colisão com a da classe será avaliada
      * */
     public boolean isHitting(Hitbox hitbox) {
-        boolean veredict = false;
+        if(hitbox.minX() > this.maxX() || hitbox.maxX() < this.minX()) {
+            return  false;
+        } else return !(hitbox.minY() > this.maxY()) && !(hitbox.maxY() < this.minY());
+    }
 
-        if(hitbox.getMinX() > this.getMaxX() || hitbox.getMaxX() < this.getMinX()) {
-            // disjunção horizontal
-            veredict = false;
-        } else if(hitbox.getMinY() > this.getMaxY() || hitbox.getMaxY() < this.getMinY()){
-            // disjunção vertical
-            veredict = false;
-        } else {
-            // intersecção não vazia
-            veredict = true;
-        }
+    /**
+     * @param position atualiza posição da hitbox
+     */
+    public void setPosition(Vector position) {
+        this.position = position;
+    }
 
-        return  veredict;
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
     }
 }
