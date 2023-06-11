2224f9ba6d6818e013599dc2ac2238f9c11a1c59
Vinicius <viniciusbsilva@usp.br>
2023-06-10 23:01:36 -0300

    :bug:  Adicionando método para compilar sem IJ

--- src/App.java
+++ src/App.java
@@ -6,6 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 class App {
+
+  public static void runGame () {
+    EventQueue.invokeLater(() -> {
+      GameFrame frame = new GameFrame(Constants.WIDTH, Constants.HEIGHT);
+      frame.setLocationRelativeTo(null); // inicia o game-loop
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+      frame.initBufferStrategy(); // inicia o buffering
+      // Cria e executa o game loop
+      GameLoop game = new GameLoop(frame);
+      game.start();
+    });
+  }
+
   public static void main(String[] args) {
 
     EventQueue.invokeLater(() -> {
