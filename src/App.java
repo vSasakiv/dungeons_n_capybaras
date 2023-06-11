7bb7566495a51ee418c292a1a94503fe8f0a0795
Sasaki <vsasakiv@usp.br>
2023-06-11 08:28:03 -0300

    :fire: remove duplicated code

--- src/App.java
+++ src/App.java
@@ -21,16 +21,6 @@ class App {
   }
 
   public static void main(String[] args) {
-
-    EventQueue.invokeLater(() -> {
-      GameFrame frame = new GameFrame(Constants.WIDTH, Constants.HEIGHT);
-      frame.setLocationRelativeTo(null); // inicia o game-loop
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setVisible(true);
-      frame.initBufferStrategy(); // inicia o buffering
-      // Cria e executa o game loop
-      GameLoop game = new GameLoop(frame);
-      game.start();
-    });
+    App.runGame();
   }
 }
