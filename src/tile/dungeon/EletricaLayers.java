11bcc88de5a66184e837629359ad29e660bb5f2f
Vinicius <viniciusbsilva@usp.br>
2023-06-27 00:51:35 -0300

    :rocket: Dungeon eletrica

--- src/tile/dungeon/EletricaLayers.java
+++ src/tile/dungeon/EletricaLayers.java
@@ -2,8 +2,16 @@ package tile.dungeon;
 
 public final class EletricaLayers extends LayerInfo{
     public EletricaLayers () {
-        fistGid = new int[]{1, 23, 1235};
-        imagePath = new String[]{"Caminho/pro/aquivo1", "Caminho/pro/arquivo2"};
+        fistGid = new int[]{1, 1, 392, 392, 1816, 1816, 2584};
+        imagePath = new String[]{
+                "/resources/Tiles/Room_Builder_free_16x16_2.png",
+                "/resources/Tiles/Room_Builder_free_16x16_2.png",
+                "/resources/Tiles/Interiors_free_16x16.png",
+                "/resources/Tiles/Interiors_free_16x16.png",
+                "/resources/Tiles/atlas_16x.png",
+                "/resources/Tiles/atlas_16x.png",
+                "/resources/Tiles/collision.png"
+        };
         TileWidth = 16;
         TileHeight = 16;
     }
