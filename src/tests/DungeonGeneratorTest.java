c47a62ee5ea07f1a5fedc10b0bdcd4cbe04789af
Fabiano Shimura <fabianoshimura@hotmail.com>
2023-07-09 17:39:01 -0300

    criando testes p dungeongen e hitbox

--- src/tests/DungeonGeneratorTest.java
+++ src/tests/DungeonGeneratorTest.java
@@ -1,7 +1,35 @@
 package tests;
 
+import java.lang.Math;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import dungeon_gen.DungeonGenerator;
+
+import org.junit.jupiter.api.DisplayName;
+
 public class DungeonGeneratorTest {
 
-    int a = 2;
+    private final DungeonGenerator dungeon_gen = new DungeonGenerator();
+
+    @Test
+    @DisplayName("Testa generate")
+    void generateTest() {
+        assertEquals(7, dungeon_gen.generate(null, 2));
+
+    }
+
+    @Test
+    @DisplayName("Testa generate")
+    void generateCorridorTest() {
+        // assertEquals(7, dungeon_gen.generateCorridor(null, null));
+
+    }
 
 }
--- src/tests/HitboxTest.java
+++ src/tests/HitboxTest.java
@@ -1,5 +1,29 @@
 package tests;
 
+import java.lang.Math;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import dungeon_gen.DungeonGenerator;
+import game_entity.Hitbox;
+
+import org.junit.jupiter.api.DisplayName;
+
 public class HitboxTest {
+
+    private final Hitbox hitbox = new Hitbox(0, 0, null);
+
+    @Test
+    @DisplayName("Testa generate")
+    void isHittingTest() {
+        assertTrue("This will succeed.", hitbox.isHitting(hitbox));
+
+    }
 
 }
