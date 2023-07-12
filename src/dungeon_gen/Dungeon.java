36f7269ac63847b3010775b4af3eafebf516c6b4
Sasaki <vsasakiv@usp.br>
2023-07-11 22:56:19 -0300

    :sparkles: add new Dungeon class

---
+++ src/dungeon_gen/Dungeon.java
@@ -0,0 +1,38 @@
+package dungeon_gen;
+
+import game_entity.mobs.EnemyTemplates;
+
+import java.util.ArrayList;
+
+public class Dungeon {
+    private MonsterRoom[] combatRooms = new MonsterRoom[7];
+    private ArrayList<int[][]> dungeonTiles;
+    private final DungeonGenerator generator = new DungeonGenerator();
+
+    public Dungeon() {}
+
+    public void geraDungeon(String tipo, int size, int maxWaves, int minEnemies, int maxEnemies){
+       this.dungeonTiles = generator.generate(tipo, size);
+       DungeonRoom[] rooms = generator.getCombatRooms();
+       for (int i = 0; i < rooms.length; i++){
+           this.combatRooms[i] = new MonsterRoom(
+                   rooms[i].getX(),
+                   rooms[i].getY(),
+                   rooms[i].getWidth(),
+                   rooms[i].getHeight(),
+                   maxWaves,
+                   minEnemies,
+                   maxEnemies,
+                   EnemyTemplates.getEnemyTemplates(),
+                   rooms[i].getValidTileMatrix());
+       }
+    }
+
+    public MonsterRoom[] getCombatRooms() {
+        return combatRooms;
+    }
+
+    public ArrayList<int[][]> getDungeonTiles() {
+        return dungeonTiles;
+    }
+}
